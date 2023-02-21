fe16016c64706edb6b11b8bcf2a14205db20335f
PSzDonat <p.sz.donat30@gmail.com>
2023-02-21 16:09:01 +0100

    vue rout kezdés nav-ok

---
+++ src/vue-Frontend/src/views/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue"
+export default {
+  name: "Game",
+  components: {NavBar}
+
+}
+</script>
+
+<style scoped>
+
+</style>
