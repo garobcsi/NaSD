f4c17c0425b3849db2a028d5474bdf0e5d4b3aaa
garobcsi <gasparrobert0908@gmail.com>
2023-02-22 18:11:50 +0100

    https change

--- src/vue-Frontend/src/assets/http.js
+++ src/vue-Frontend/src/assets/http.js
@@ -1,1 +1,12 @@
-// here goes axios config
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 4KXakKYnBSPJMfGFjNj04qhAYR0CP4O0KHAMkmSg'
+    }
+});
+
+export {http}
